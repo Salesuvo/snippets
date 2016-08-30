0cb2516ce3511c2f6382d750195e77b9895655de
Volker Weber <vweber@STEPHANXP.local>
2016-08-30 15:34:28 +0200

    rework of swapMagic

--- swapMagic.c
+++ swapMagic.c
@@ -6,30 +6,30 @@ unsigned char swapbyte(unsigned char byteToSwap)
     return byteToSwap;
 }
 
-uint32_t uByteSwap_32(uint32_t myWord)
+uint32_t uSwapByte32(uint32_t myWord)
 {
     return ((myWord << 24) | ((myWord << 8) & 0x00FF0000) | ((myWord >> 8) & 0x0000FF00) | ((myWord >> 24) & 0x000000FF));
 }
 
-uint32_t uByteSwap_24(uint32_t myWord)
+uint32_t uSwapByte24(uint32_t myWord)
 {
     return (((myWord << 16) | (myWord & 0xFF00) | ((myWord >> 16) & 0xFF)) & 0x00FFFFFF);
 }
 
-uint16_t uByteSwap_16(uint16_t myWord)
+uint16_t uSwapByte16(uint16_t myWord)
 {
     return ((myWord << 8) | ((myWord >> 8) & 0xFF));
 }
 
-int32_t sByteSwap_32(int32_t myWord)
+int32_t sSwapByte32(int32_t myWord)
 {
     return ((myWord << 24) | ((myWord << 8) & 0x00FF0000) | ((myWord >> 8) & 0x0000FF00) | ((myWord >> 24) & 0x000000FF));
 }
 
-int32_t sByteSwap_24(int32_t myWord)
+int32_t sSwapByte24(int32_t myWord)
 {
     int32_t myResult = ((myWord << 16) | (myWord & 0xFF00) | ((myWord >> 16) & 0xFF)) & 0x00FFFFFF;
-    // Add sign extension when needed
+    // if needed : add sign extension
     if ((myResult & 0x00800000) == 0x00800000)
     {
         myResult |= 0xFF000000;
@@ -37,7 +37,7 @@ int32_t sByteSwap_24(int32_t myWord)
     return (myResult);
 }
 
-int16_t sByteSwap_16(int16_t myWord)
+int16_t sSwapByte16(int16_t myWord)
 {
     return ((myWord << 8) | ((myWord >> 8) & 0xFF));
-}
+} 
