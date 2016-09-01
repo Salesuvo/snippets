3d47e7873cd2f9b36e217ee73c3194aaa67b66f3
Volker Weber <vweber@STEPHANXP.local>
2016-09-01 11:03:36 +0200

    added bitManipulation

---
+++ bitManipulation.c
@@ -0,0 +1,15 @@
+// use the bitwise OR operator (|) to set a bit. That will set bit x.
+number |= 1 << x;
+
+// Clearing a bit, use the bitwise AND operator (&) to clear a bit.
+// That will clear bit x. You must invert the bit string with the bitwise NOT operator (~), then AND it.
+number &= ~(1 << x);
+
+// Toggling a bit. The XOR operator (^) can be used to toggle a bit.
+number ^= 1 << x;
+
+// Checking a bit, shift the number x to the right, then bitwise AND it. That will put the value of bit x into the variable bit.
+bit = (number >> x) & 1;
+
+// Setting the nth bit to either 1 or 0 can be achieved with the following:
+number ^= (-x ^ number) & (1 << n);
