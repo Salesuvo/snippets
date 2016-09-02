ec4c1ed6d90e59d0fa7519a4e952865efbed0004
Volker Weber <vweber@STEPHANXP.local>
2016-09-02 08:16:57 +0200

    rework of rand

--- randomNumber.c
+++ randomNumber.c
@@ -2,15 +2,19 @@
  * Numerical Recipes in C: The Art of Scientific Computing (William H. Press, Brian P. Flannery, Saul A. Teukolsky, William T. Vetterling; New York: Cambridge University Press, 1992 (2nd ed., p. 277))
  */
 
-// If you want to generate a random integer between 1 and 10, you should always do it by using high-order bits, as in
-j=1+(int) (10.0*rand()/(RAND_MAX+1.0));
+     // ToDo: check for using stdlib here is memory save
+ 
+// if you want to generate a random integer between 1 and 10, you should always do it by using high-order bits, as in
+myRandomNumber = 1 + (int)(10.0F * rand() / (RAND_MAX + 1.0F));
 
 // and never by anything resembling,(which uses lower-order bits).
-j=1+(rand() % 10);
+myRandomNumber = 1 + (rand() % 10);
 
 // to yield a pseudo-random number in a [min,max] range:
 unsigned int randr(unsigned int min, unsigned int max)
 {
-    double scaled = rand() / (RAND_MAX + 1.0);
+    // from YON1: think about using srand() here
+
+    double scaled = rand() / (RAND_MAX + 1.0F);
     return (max - min + 1) * scaled + min;
 }
